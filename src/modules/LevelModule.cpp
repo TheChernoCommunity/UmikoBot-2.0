ec8116a9d65a1addfd6097bf489f14bf1c97af64
FancyKillerPanda <fancykillerpanda@gmail.com>
2021-10-17 15:08:27 +1100

    Add xp on each message.

--- src/modules/LevelModule.cpp
+++ src/modules/LevelModule.cpp
@@ -7,6 +7,23 @@ using namespace Discord;
 LevelModule::LevelModule()
 	: Module("Level")
 {
+	messageXpTimer.setInterval(30 * 1000);
+	messageXpTimer.start();
+	QObject::connect(&messageXpTimer, &QTimer::timeout, [this]()
+	{
+		for (QList<UserLevelData>& guildLevelData : levelData)
+		{
+			for (UserLevelData& userLevelData : guildLevelData)
+			{
+				if (userLevelData.messageCount > 0)
+				{
+					userLevelData.currentXp += 10 + (qrand() % 6);
+					userLevelData.messageCount = 0;
+				}
+			}
+		}
+	});
+	
 	namespace CP = CommandPermission;
 }
 
@@ -24,4 +41,20 @@ void LevelModule::onLoad(const QJsonObject& mainObject)
 
 void LevelModule::onMessage(const Message& message, const Channel& channel)
 {
+	getUserLevelData(channel.guildId(), message.author().id()).messageCount += 1;
+}
+
+UserLevelData& LevelModule::getUserLevelData(GuildId guildId, UserId userId)
+{
+	for (UserLevelData& userLevelData : levelData[guildId])
+	{
+		if (userLevelData.userId == userId)
+		{
+			return userLevelData;
+		}
+	}
+
+	// The user does not exist yet, make a new one
+	levelData[guildId].append(UserLevelData { userId });
+	return levelData[guildId].back();
 }
--- src/modules/LevelModule.h
+++ src/modules/LevelModule.h
@@ -3,6 +3,13 @@
 #include "core/Core.h"
 #include "modules/Module.h"
 
+struct UserLevelData
+{
+	UserId userId = 0;
+	int currentXp = 0;
+	int messageCount = 0; // Resets every 30 seconds
+};
+
 class LevelModule : public Module
 {
 public:
@@ -14,4 +21,11 @@ protected:
 	void onLoad(const QJsonObject& mainObject) override;
 
 	void onMessage(const Discord::Message&, const Discord::Channel&);
+
+private:
+	UserLevelData& getUserLevelData(GuildId guildId, UserId userId);
+	
+private:
+	QTimer messageXpTimer;
+	QMap<GuildId, QList<UserLevelData>> levelData;
 };
